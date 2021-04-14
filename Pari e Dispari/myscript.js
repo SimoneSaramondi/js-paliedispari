e03cb1bbabe4bae78071710f177709ba9bf7940b
SimoneSaramondi <simo98sara@gmail.com>
2021-04-14 16:03:31 +0200

    final project

--- Pari e Dispari/myscript.js
+++ Pari e Dispari/myscript.js
@@ -1,10 +1,41 @@
 var pariODispari = prompt("Scegli se vuoi immettere un numero Pari o un numero Dispari");
 
+//Pari o Dispari
 var first = pariODispari.slice(0, 1).toUpperCase();
 var second = pariODispari.slice(1);
 pariODispari = first + second;
-console.log(pariODispari);
 
-if(pariODispari != "Pari" || pariODispari != "Dispari"){
-    console.log("Hai sbagliato a digitare Pari o Dispari");
+var numeroUmano;
+
+if(pariODispari === "Pari"){
+    console.log("Hai selezionato Pari");
+    numeroUmano = parseInt(prompt("Ora scegli che numero Pari vuoi"));
+    while(numeroUmano % 2 != 0){
+        numeroUmano = parseInt(prompt("Il numero non era Pari, rimettilo"));
+    }
+    console.log("Il tuo numero è: " + numeroUmano);
+}else if(pariODispari === "Dispari"){
+    console.log("Hai selezionato Dispari");
+    numeroUmano = parseInt(prompt("Ora scegli che numero Dispari vuoi"));
+    while(numeroUmano % 2 == 0){
+        numeroUmano = parseInt(prompt("Il numero non era Dispari, rimettilo"));
+    }
+    console.log("Il tuo numero è: " + numeroUmano);
 }
+
+var numeroComputer = Math.floor(Math.random() * 5) + 1;
+console.log("Il numero del Computer è: " + numeroComputer);
+
+var somma = numeroComputer + numeroUmano;
+console.log("La somma dei numeri è: " + somma);
+
+//Funzione che restituisce se la somma è pari o dispari
+function typeNum(numeroSomma){
+    if(numeroSomma % 2 == 0){
+        return console.log("La somma dei numeri è Pari");
+    }else {
+        return console.log("La somma dei numeri è Dispari");
+    }
+}
+
+var risultato = typeNum(somma);
